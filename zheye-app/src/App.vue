f2291072df73bf7c2e092308050cc9c74a97b6fd
yashoku <70477548+yashoku233@users.noreply.github.com>
2023-05-14 00:40:04 +0800

    no local

--- zheye-app/src/App.vue
+++ zheye-app/src/App.vue
@@ -1,18 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="contariner">
+    <column-list :list="list" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { defineComponent } from 'vue';
+import ColumnList from './components/ColumnList.vue';
+import { ColumnProps } from './types/ColumnProps'
+
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1',
+    description: '这是test1的专栏,有一段非常有意思的简介',
+    avatar: 'dad'
+  },
+  {
+    id: 2,
+    title: 'test2',
+    description: '这是test2的专栏,有一段非常有意思的简介',
+    avatar: 'dad'
+  },
+  {
+    id: 3,
+    title: 'test3',
+    description: '这是test3的专栏,有一段非常有意思的简介',
+    avatar: 'dad'
+  },
+  {
+    id: 4,
+    title: 'test4',
+    description: '这是test4的专栏,有一段非常有意思的简介',
+    avatar: 'dad'
+  }
+]
 export default defineComponent({
-  name: 'App',
   components: {
-    HelloWorld
+    ColumnList,
+  },
+  setup() {
+    return {
+      list: testData
+    }
   }
 })
+
 </script>
 
 <style>
---
+++ zheye-app/src/components/ColumnList.vue
@@ -0,0 +1,32 @@
+<template >
+    <div class="row">
+        <div v-for="column in list" :key="column.id" class="col-4 mb-4">
+            <div class="card h-100 shadow-sm">
+                <div class="card-body">
+                    <img :src="column.avatar" class="round-circle boder-light w-25 my-3" :alt="column.title">
+                    <h5 class="card-title">{{ column.title }}</h5>
+                    <p class="card-text text-left">{{ column.description }}</p>
+                    <a href="#" class="btn btn-primary">{{ '进入专栏' }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+
+import { defineComponent, PropType } from "vue";
+import { ColumnProps } from '../types/ColumnProps'
+
+export default defineComponent({
+    name: "ColumList",
+    props: {
+        list: {
+            type: Array as PropType<ColumnProps[]>,
+            required: true,
+        },
+    },
+});
+</script>
+<style></style>
