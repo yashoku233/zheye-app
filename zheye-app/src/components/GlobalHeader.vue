b49fa5d8e35fa2696fdd41a2c1141a89ca6e6041
yashoku <70477548+yashoku233@users.noreply.github.com>
2023-10-22 00:05:41 +0800

    更新 EsLint的规则

--- zheye-app/src/components/GlobalHeader.vue
+++ zheye-app/src/components/GlobalHeader.vue
@@ -1,24 +1,23 @@
 <template>
-    <h1></h1>
+  <h1></h1>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 
 export interface UserProps {
-    isLogin: boolean;
-    name?: string;
-    id?: number;
+  isLogin: boolean;
+  name?: string;
+  id?: number;
 }
 
 export default defineComponent({
-    name: 'GlobalHeader',
-    props: {
-        user: {
-            type: Object as PropType<UserProps>,
-            required: true,
-        }
-    }
-})
-
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true,
+    },
+  },
+});
 </script>
